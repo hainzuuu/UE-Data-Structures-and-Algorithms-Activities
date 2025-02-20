8a2ae013921e92affcd246819d22b43ff1becfcb
John Hynes Longares <johnhyneslongares@gmail.com>
2025-02-20 09:48:58 +0800

    Update set-data-structure-and-set-operations.py

--- set-data-structure-and-set-operations.py
+++ set-data-structure-and-set-operations.py
@@ -19,8 +19,6 @@ print("____________________")
 print("Union of sets (A and C): ", A | C)
 print("Intersection of sets (A and B): ", A & B)
 print("Symmetric difference of sets (B and C): ", B ^ C)
-print("____________________")
-print("name: Longares, John Hynes C.")
-print("student no.: 20211121312")
+
 
 
